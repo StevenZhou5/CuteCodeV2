8141206efdbf0728eeec4aa32b711582e7247980
stevenzhouzhenwu <stevenzhouzhenwu@didichuxing.com>
2021-07-18 14:37:37 +0800

    上传迁移的CuteCode代码

--- Go/01-Study/wgTest.go
+++ Go/01-Study/wgTest.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"wg_test"
+	"fmt"
+	"time"
 )
 
 func main() {
 	//fmt.Printf("生成随机数：%+v\n", rand.Intn(100))
-	wg_test.Test01()
+	//wg_test.Test01()
+	fmt.Printf("start\n")
+	//for range time.Tick(time.Second * 5) {
+	//	fmt.Printf("1111")
+	//}
+	hour := time.Now().Unix() % 86400 / 3600
+	fmt.Printf("end:%v,\n", hour)
 }
